88e2793acd9c339af2411bceb9ce3d795eb011c8
Adrián Nicolás Yáñez Viceiro <yanvicadr@IESPIRINGALLA.AD>
2023-02-14 19:20:04 +0100

    Archivo .cs ClaseMeteoG añadido

---
+++ Assets/Script/ClasesMeteoG.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ClaseMeteoG {
+    public string concello;
+    public string dataUltimaAct;
+    public string idConcello;
+    public string identificador;
+    public string imaxenCamara;
+    public string imaxenCamaraMini;
+    public string lat;
+    public string lon;
+    public string nomeCamara;
+    public string provincia;
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour {
-    // Start is called before the first frame update
     void Start() {
-        
+        // A correct website page.
+        StartCoroutine(GetRequest("https://servizos.meteogalicia.gal/mgrss/observacion/jsonCamaras.action"));
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
     }
 }
